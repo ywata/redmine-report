0984f82a9d810c9e27a76a300b0985984deb04be
Yasuhiko Watanabe <ywataywatay@gmail.com>
2015-03-25 22:19:00 +0900

    Comment out test code.

--- src/Redmine/Utils.hs
+++ src/Redmine/Utils.hs
@@ -10,8 +10,8 @@ parseRHTime = parseTime defaultTimeLocale "%FT%X%QZ"
 
 parseShortTime :: String -> Maybe Day
 parseShortTime = parseTime defaultTimeLocale "%F"
-
+{-
 a = parseRHTime "2015-10-11T02:00:00Z"
 b = parseRHTime "2015-10-11T03:00:00Z"
 x = parseShortTime "2015-10-11"
-
+-}
