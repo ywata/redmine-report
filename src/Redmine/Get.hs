ceedfa29f81e1954512f6f74ad1b7f990950decf
Yasuhiko Watanabe <ywataywatay@gmail.com>
2015-06-04 07:10:55 +0900

    Temporalycommit common code.

---
+++ src/Redmine/Get.hs
@@ -0,0 +1,110 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Redmine.Get ( getTimeEntries
+                   , getTimeEntry
+                   , getTimeEntriesForIssue
+                   , getIssue
+                   , getIssues
+                   , getProjects
+                   , getProjectForId
+                   , getProject
+                   , getTrackers
+                   , getVersions
+                   , getUser
+                   , expandOptions
+                   , increaseQueryRange
+                   ) where
+
+import Data.Monoid
+import qualified Data.Map as Map
+import Redmine.JSON
+import Redmine.Types
+import Redmine.Manager
+import Redmine.Rest
+import qualified Data.ByteString as S
+import qualified Data.ByteString.Char8 as S8
+import Network.HTTP.Conduit
+import Network.HTTP.Base (RequestMethod(..))
+import Network.HTTP.Client.TLS
+import Control.Monad.IO.Class (liftIO)
+import Control.Monad.Trans.Maybe
+import Debug.Trace
+
+
+
+-- |The function 'getTimeEntries' fetches all the time entries.
+--  They can be filtered by spenton date using spent_on=%3E%3C2013-05-01|2013-05-31
+getTimeEntries :: RedmineMng -> ParamRest -> MaybeT IO [TimeEntry]
+getTimeEntries mng param = MaybeT $ do
+   mngConn <- newManager tlsManagerSettings
+   res <- queryRedmineAvecOptions mng GET requete ( Map.union param initOpt) Nothing mngConn 
+   return $ fmap time_entries res
+   where requete = "/time_entries.json"
+
+getTimeEntry :: RedmineMng -> Integer -> MaybeT IO TimeEntry
+getTimeEntry mng elemId = 
+   fmap time_entry (MaybeT $ runQuery mng GET requete Nothing)
+   where requete = "/time_entries/" <> S8.pack (show elemId) <> ".json"
+                
+getTimeEntriesForIssue :: RedmineMng -> Integer-> MaybeT IO [TimeEntry]
+getTimeEntriesForIssue mng issueid = MaybeT $ do
+   mngConn <- newManager tlsManagerSettings
+   res <- queryRedmineAvecOptions mng GET requete initOpt Nothing mngConn
+   return $ fmap time_entries res
+   where requete = "/time_entries/" <> S8.pack (show issueid) <> ".json"
+
+getIssues :: RedmineMng -> ParamRest -> MaybeT IO [Issue]
+getIssues mng param = MaybeT $ do
+   mngConn <- liftIO $ newManager tlsManagerSettings
+   res <- queryRedmineAvecOptions mng GET requete ( Map.union param initOpt) Nothing mngConn
+   return $ fmap issues res
+   where requete = "/issues.json"
+
+getIssue :: RedmineMng -> Integer -> ParamRest -> MaybeT IO Issue
+getIssue mng elemId param =
+   fmap issue (MaybeT $ runQuery mng GET requete Nothing)
+   where requete = "/issues/" <> S8.pack (show elemId) <> ".json" <> expandOptions param
+
+getTrackers :: RedmineMng -> MaybeT IO [Tracker]
+getTrackers mng = MaybeT $ runQuery mng GET (trace (show requete) requete) Nothing
+  where requete = rmhost mng <> "/trackers.json" 
+--   mngConn <- newManager tlsManagerSettings
+--   res <- queryRedmineAvecOptions mng GET requete initOpt Nothing mngConn 
+--   return $ fmap trackers res
+--   where requete = "/trackers.json"
+
+getProjects :: RedmineMng -> MaybeT IO [Project]
+getProjects mng = MaybeT $ do
+   mngConn <- newManager tlsManagerSettings
+   res <- queryRedmineAvecOptions mng GET requete initOpt Nothing mngConn 
+   return $ fmap projects res
+   where requete = "/projects.json"
+
+getProjectForId :: RedmineMng -> Integer -> MaybeT IO Project
+getProjectForId mng elemId =
+   MaybeT $ runQuery mng GET requete Nothing
+   where requete = rmhost mng <> "/projects/" <> S8.pack (show elemId) <> ".json"
+
+getProject :: RedmineMng -> S.ByteString -> MaybeT IO Project
+getProject mng projId =
+   MaybeT $ runQuery mng GET requete Nothing
+   where requete = "/projects/" <> projId <> ".json"
+
+--Get all the versions associated to a project
+getVersions :: RedmineMng --The connection manager
+            -> S.ByteString --The project
+            -> MaybeT IO [Version]
+getVersions mng proj =
+   fmap versions (MaybeT $ runQuery mng GET requete Nothing)
+   where requete = "/projects/" <> proj <> "/versions.json"
+
+getVersion:: RedmineMng -> Integer -> ParamRest -> MaybeT IO Version
+getVersion mng elemId param =
+   fmap version (MaybeT $ runQuery mng GET requete Nothing)
+   where requete = "/versions/" <> S8.pack (show elemId) <> ".json" <> expandOptions param
+
+getUser :: RedmineMng -> Integer -> MaybeT IO User
+getUser mng elemId =
+    fmap user (MaybeT $ runQuery mng GET requete Nothing)
+    where requete = "/users/" <> S8.pack (show elemId) <> ".json"
+
---
+++ src/Redmine/Types.hs
@@ -0,0 +1,238 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Redmine.Types where
+
+import Data.Time.Clock      (UTCTime)
+import Data.Time.Calendar (Day)
+import Network.HTTP.Conduit
+import Data.Monoid
+import Data.Typeable
+import Data.Data
+import Data.Aeson
+import qualified Data.Text as T
+
+data Status = Closed | Open deriving (Eq, Show)
+
+data VersionsRsp = VersionsRsp { versions :: [Version] } deriving (Eq,Show)
+
+data VersionRsp = VersionRsp { version :: Version } deriving (Eq,Show)
+
+data Version = Version { id_Version      :: Integer
+                       , name_Version    :: T.Text
+                       , project_Version :: ObjRef
+                       , desc_Version    :: T.Text
+                       , status_Version  :: T.Text
+                       , sharing_Version :: T.Text
+                       , dueDate_Version     :: Maybe Day
+                       , createdOn_Version   :: Maybe UTCTime
+                       , updatedOn_Version   :: Maybe UTCTime
+                       } deriving (Eq, Show)
+--ajouter offset total_cout et limit
+data IssuesRsp = IssuesRsp { issues :: [Issue] } deriving (Eq,Show)
+
+--data Parent = Parent { parent :: ObjID } deriving (Eq,Show)
+
+data IssueRsp = IssueRsp { issue :: Issue } deriving (Eq,Show)
+
+data Issue = Issue { id_Issue :: Integer
+                   , project_Issue :: ObjRef
+                   , parent_Issue :: Maybe ObjID --Single issue only
+                   , tracker_Issue :: ObjRef
+                   , status_Issue :: ObjRef
+                   , priority_Issue :: ObjRef
+                   , author_Issue :: ObjRef
+                   , assignedTo_Issue :: Maybe ObjRef
+                   , category_Issue :: Maybe ObjRef
+                   , fixedVersion_Issue :: Maybe ObjRef
+                   , subject_Issue :: T.Text
+                   , description_Issue :: T.Text
+                   , startDate_Issue :: Maybe Day
+                   , dueDate_Issue :: Maybe Day
+                   , doneRatio_Issue :: Int
+                   , estimatedHours_Issue :: Maybe Float
+                   , spentHours_Issue :: Maybe Float --Single issue only
+                   , customFields_Issue :: Maybe [CustomField]
+                   , createdOn_Issue   :: Maybe UTCTime
+                   , updatedOn_Issue   :: Maybe UTCTime
+                   , journals_Issue :: Maybe [Journal] -- Single issue only
+                   , attachements_Issue :: Maybe [Attachement]
+                   , changeSets_Issue :: Maybe [ChangeSet]
+                   , watchers_Issue :: Maybe [Watcher]
+                   , relations_Issue :: Maybe [Relation]
+                   , children_Issue :: Maybe [Child]
+                   } deriving (Eq, Show)
+
+data ChangeSet = ChangeSet { revision_ChangeSet :: T.Text
+                           , user_ChangeSet :: ObjRef
+                           , comments_ChangeSet :: T.Text
+                           , committedOn_ChangeSet :: UTCTime
+                           } deriving (Eq, Show)
+
+data Watcher = Watcher { id_Watcher :: Integer
+                       , name_Watcher :: T.Text
+                       } deriving (Eq, Show)
+
+data Child = Child { id_Child :: Integer
+                   , tracker_Child :: ObjRef
+                   , subject_Tracker :: T.Text
+                   } deriving (Eq, Show)
+
+data CustomField = CustomField { id_CF    :: Integer
+                               , name_CF  :: T.Text
+                               , value_CF :: T.Text
+                               } deriving (Eq,Show)
+
+data ObjRef = ObjRef { id_ObjRef:: Integer, name_ObjRef:: T.Text } deriving (Eq, Show)
+
+data ObjID = ObjID { id_ObjID:: Integer } deriving (Eq, Show)
+
+data IssueStatuses = IssueStatuses { issue_statuses :: [IssueStatus] }
+                                     deriving (Eq,Show)
+
+data IssueStatus = IssueStatus { id_IssueStatus:: Integer
+                               , name_IssueStatus:: T.Text
+                               , isDefault_IssueStatus:: Bool
+                               , isClosed_IssueStatus:: Bool
+                               } deriving (Eq, Show)
+
+data ProjectsRsp = ProjectsRsp { projects :: [Project] } deriving (Eq,Show)
+
+data ProjectRsp = ProjectRsp { project :: Project } deriving (Eq,Show)
+
+data Project = Project { id_Project:: Integer
+                       , name_Project:: T.Text
+                       , identifier_Project:: T.Text
+                       , desc_Project:: T.Text
+                       , customFields_Project :: Maybe [CustomField]
+                       , createdOn_Project   :: Maybe UTCTime
+                       , updatedOn_Project   :: Maybe UTCTime
+                       } deriving (Eq, Show)
+
+
+data UsersRsp = UsersRsp { users :: [User] } deriving (Eq,Show)
+
+data UserRsp = UserRsp { user :: User } deriving (Eq,Show)
+
+data User = User { lastname:: T.Text
+                 , createdOn_User:: Maybe UTCTime
+                 , mail:: T.Text
+                 , r :: Maybe UTCTime
+                 , firstname:: T.Text
+                 , id_User:: Integer
+                 } deriving (Eq, Show)
+
+data TrackersRsp = TrackersRsp { trackers :: [Tracker] } deriving (Eq,Show)
+
+data TrackerRsp = TrackerRsp { tracker :: Tracker } deriving (Eq,Show)
+
+data Tracker = Tracker { id_Tracker   :: Integer
+                       , name_Tracker :: T.Text
+                       } deriving (Eq, Show)
+
+
+data Detail = Detail { property :: T.Text
+                     , name_Detail :: T.Text
+                     , old_value_Detail :: Maybe T.Text
+                     , new_value_Detail :: T.Text
+                     } deriving (Eq,Show)
+
+
+data Journal = Journal { id_Journal :: Integer
+                       , user_Journal :: ObjRef
+                       , notes_Journal :: T.Text
+                       , createdOn_Journal :: Maybe UTCTime
+                       , details_Journal :: Maybe [Detail]
+                       } deriving (Eq, Show)
+
+
+data Attachement = Attachement { id_Attachement :: Integer
+                               , filename_Attachement :: T.Text
+                               , filesize_Attachement :: Integer
+                               , contentType_Attachement :: T.Text
+                               , description_Attachement :: T.Text
+                               , contentUrl_Attachement :: T.Text
+                               , authorName_Attachement :: ObjRef
+                               , createdOn_Attachement :: UTCTime
+                               } deriving (Eq, Show)
+
+
+data TimeEntriesRsp = TimeEntriesRsp { time_entries :: [TimeEntry] } deriving (Eq,Show)
+
+data TimeEntryRsp = TimeEntryRsp { time_entry :: TimeEntry } deriving (Eq,Show)
+
+data TimeEntry = TimeEntry { id_TE       :: Integer
+                           , project_TE  :: ObjRef
+                           , issue_TE      :: ObjID
+                           , user_TE      :: ObjRef
+                           , activity_TE   :: Maybe ObjRef
+                           , hours_TE      :: Maybe Float
+                           , comments_TE   :: T.Text
+                           , createdOn_TE   :: Maybe UTCTime
+                           , updatedOn_TE   :: Maybe UTCTime
+                           , spentOn_TE   :: Maybe Day
+                           } deriving (Eq, Show)
+
+data Memberships = Memberships { memberships :: [Membership]
+                               } deriving (Eq,Show)
+
+data Membership = Membership { id_Membership :: Integer
+                             , project_Membership :: ObjRef
+                             , user_Membership    :: ObjRef
+                             , roles_Membership   :: [Role]
+                             } deriving (Eq, Show)
+
+data Roles = Roles { roles :: [Role] } deriving (Eq,Show)
+
+data Role = Role { id_Role   :: Integer
+                 , name_Role :: T.Text
+                 } deriving (Eq, Show)
+
+data RelationType = Relates | Duplicates | Duplicated | Blocks | Blocked
+                  | Precedes | Follows deriving (Eq, Show)
+
+data Relations = Relations { relations :: [Relation] } deriving (Eq,Show)
+
+data Relation = Relation { id_Relation :: Integer
+                         , issueId_Relation:: Integer
+                         , issueToId_Relation:: Integer
+                         , relationType_Relation:: T.Text
+                         , delay_Relation:: Maybe Integer
+                         } deriving (Eq, Show)
+
+instance Monoid TimeEntriesRsp where
+ mappend a b = TimeEntriesRsp (time_entries a++time_entries b)
+ mempty      = TimeEntriesRsp []
+
+instance Monoid IssuesRsp where
+ mappend a b = IssuesRsp (issues a++issues b)
+ mempty      = IssuesRsp []
+
+instance Monoid ProjectsRsp where
+ mappend a b = ProjectsRsp (projects a++projects b)
+ mempty      = ProjectsRsp []
+
+instance Monoid TrackersRsp where
+ mappend a b = TrackersRsp (trackers a ++ trackers b)
+ mempty      = TrackersRsp []
+
+instance Monoid VersionsRsp where
+ mappend a b = VersionsRsp (versions a++versions b)
+ mempty      = VersionsRsp []
+
+class Collection a where
+    longueur :: a -> Int
+
+instance Collection TimeEntriesRsp where
+    longueur (TimeEntriesRsp a) = length a
+
+instance Collection TrackersRsp where
+    longueur (TrackersRsp a) = length a
+
+instance Collection IssuesRsp where
+    longueur (IssuesRsp a) = length a
+
+instance Collection ProjectsRsp where
+    longueur (ProjectsRsp a) = length a
+
+instance Collection VersionsRsp where
+    longueur (VersionsRsp a) = length a
+
